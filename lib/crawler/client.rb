ed2bca7b32cf0125d38771b750b28cfbbf3f9ad9
Brian Kane <brian.l.kane@gmail.com>
2015-03-25 12:40:29 -0400

    Add basic site map tests

--- lib/crawler/client.rb
+++ lib/crawler/client.rb
@@ -4,10 +4,21 @@ module Crawler
   class Client
 
     def initialize(url)
+      @page = Page.new url
     end
 
     def site_map
-      Nokogiri::HTML::Builder.new(encoding: 'utf-8').to_html
+      doc = StringIO.new
+      doc << '<html>'
+      pages.each do |page|
+        doc << "<div id=\"#{page.url}\" title=\"#{page.title}\"></div>"
+      end
+      doc << '</html>'
+      doc.string
+    end
+
+    def pages
+      @page.linked_pages
     end
   end
 end
--- spec/crawler_spec.rb
+++ spec/crawler_spec.rb
@@ -6,22 +6,35 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/cassettes"
   config.hook_into :typhoeus
   config.configure_rspec_metadata!
+  config.default_cassette_options = {:record => :new_episodes}
 end
 
 describe Crawler do
 
   let(:url) { 'http://xkcd.com' }
-  subject(:crawler) { Crawler.new url }
 
   it 'accepts a URL' do
-    expect { crawler }.to_not raise_error
+    VCR.use_cassette('site_map') do
+      expect { Crawler.new url }.to_not raise_error
+    end
   end
 
   describe 'site map generation' do
 
     it 'generates an HTML document' do
-      site_map = crawler.site_map
-      expect { Nokogiri::HTML site_map }.to_not raise_error
+      VCR.use_cassette('site_map') do
+        crawler = Crawler.new url
+        site_map = crawler.site_map
+        expect { Nokogiri::HTML site_map }.to_not raise_error
+      end
+    end
+
+    it 'contains one <div> per page', :vcr do
+      VCR.use_cassette('site_map') do
+        crawler = Crawler.new url
+        site_map = Nokogiri::HTML crawler.site_map
+        expect(site_map.xpath('//div').count).to eq 14
+      end
     end
   end
-end   
+end
